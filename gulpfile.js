5e54ac7a799c54f2149b9a9ec257dbbd3eb5ed62
Krekotun <a.krekotun@gmail.com>
2014-07-07 22:43:45 +0300

    return stylus task

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp'),
     stylus = require('gulp-stylus');
 
 gulp.task('stylus', function() {
-    gulp.src('./src/assets/styles/style.styl')
+    return gulp.src('./src/assets/styles/style.styl')
         .pipe(stylus({
             compress: true
         }))
